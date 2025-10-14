aeafd3fa08ee4b36d5f6ff72903bd8c6621ef65f
Sh Srkh <216752569+shsrkh@users.noreply.github.com>
2025-10-14 15:38:43 +0200

    feat: add weather data fetch using OpenWeather API

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const cityInput = document.querySelector('.city-input')
 const searchBtn = document.querySelector('.search-btn')
 
+const apiKey = '18ebc0270ccd46c7fe623fded5bcc087'
+
 searchBtn.addEventListener('click', () => {
     if (cityInput.value.trim() != '') {
         updateWeatherInfo(cityInput.value)
@@ -17,6 +19,15 @@ cityInput.addEventListener('keydown', (event) => {
     }
 })
 
-function updateWeatherInfo(city) {
-    
+async function getFetchData(endPoint, city) {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/${endPoint}?q=${city}&appid=${apiKey}&units=metric`
+
+    const response = await fetch(apiUrl)
+
+    return response.json()
+}
+
+async function updateWeatherInfo(city) {
+    const weatherData = await getFetchData('weather', city)
+    console.log(weatherData)
 }
